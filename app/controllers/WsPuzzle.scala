b0919a73c9ada85b5a375f82568f034495bc9979
John Langley <digerat@gmail.com>
2014-01-19 11:33:40 -0500

    Revert the fix. Look for potential solutions to this puzzle on the   "fix" branch. The master contains the original puzzle.
    This reverts commit 552a4e4b088804f99aa32b625d3d68e14d1017f5.

--- app/controllers/WsPuzzle.scala
+++ app/controllers/WsPuzzle.scala
@@ -20,55 +20,33 @@ object WsPuzzle extends Controller  {
   def wsPostWithIterators(remoteUrl: String) = Action.async { implicit request =>
     // create a channel that is connected to an enumerator which is returned in the result
     val (resultEnumerator, channelForIteratee) = Concurrent.broadcast[Array[Byte]]
-    channelForIteratee.push("Never an empty enumerator".getBytes)
     // define our consumer a.k.a. our iteratee -- It just pushes into the channel for the enumerator returned in the response
     val resultIteratee = Iteratee.foreach[Array[Byte]] { chunk =>  channelForIteratee.push(chunk)   }
     // WS doesn't end the iteratee so w/out the channelForIteratee.eofAndEnd the response never terminates
-    val postResultFuture = WS.url(remoteUrl).postAndRetrieveStream(request.body.asJson.getOrElse(new JsObject(Seq()))) { 
+    val postResultFuture = WS.url(remoteUrl).postAndRetrieveStream(request.body.asJson.getOrElse(new JsObject(Seq()))){ 
       headers => resultIteratee 
     }.map {it =>  
-        channelForIteratee.push(Input.EOF)
-        Future{ it.run }
-    }
-    Await.result(postResultFuture, 2 seconds)
-    Future { Ok.chunked(resultEnumerator).withHeaders(("Content-Type" -> "text/plain")) }
-  }
-  
-  def wsPostWithIteratorsWithStream(remoteUrl: String) = Action { implicit request =>
-    import java.io.{OutputStream, BufferedOutputStream, FileOutputStream, ByteArrayOutputStream}
-	val outputStream: OutputStream = new ByteArrayOutputStream()
-	outputStream.write("We always have a non-empty stream. ".getBytes());
-	def fromStream(stream: OutputStream): Iteratee[Array[Byte], Unit] = Cont {
-	  case e@Input.EOF =>
-	    stream.flush(); stream.close()
-	    Logger.info(s"iteratee is at EOF, outputStream contents = ${outputStream}")
-	    Done((), e)
-	  case Input.El(data) =>
-	    Logger.info(s">>> data pushed into stream = ${new String(data)}")
-	    stream.write(data)
-	    fromStream(stream)
-	  case Input.Empty =>
-	    fromStream(stream)
-	}
-	val futureResponse = WS.url(remoteUrl).postAndRetrieveStream(request.body.asJson.getOrElse(new JsObject(Seq()))) {
-	  headers => fromStream(outputStream)
-	}.map {it8ee => Future {it8ee.run} }
-	Await.result(futureResponse, 2 seconds)
-	val out = outputStream.toString
-	Logger.info(">>>> out = " + out)
-	Ok(outputStream.toString())
+   	  resultIteratee.run
+      val x = Await.result (
+        Future { 
+          Ok.chunked(resultEnumerator).withHeaders(("Content-Type" -> "text/plain"))
+        }, 2 seconds
+      )
+      x
+    }// .andThen{case r => channelForIteratee.eofAndEnd; }
+    postResultFuture
   }
 
   // Called from my wsPostWithIterators action as a test. This is just a test fixture
   def postTestEndpoint = Action { implicit request =>
     // Thread.sleep(1000) // fake delay for latency simulation -- doesn't make any difference 
-  	Ok("Here is a response @ " + new java.util.Date() + " and your body was: " + request.body)
+  	Ok("here is a response @ " + new java.util.Date() + " and your body was: " + request.body)
   }
 
   // This is my test entry point... it calls the "system under test" aka the wsPostWithIterators which is
   // calling the "test fixture" postTestEndpoint
-  def getPostEndpoint(remoteUrl: String) = Action.async { implicit request => 
-    WS.url(remoteUrl).post("ghostBody").map { response =>
+  def getPostEndpoint = Action.async { implicit request => 
+    WS.url("http://localhost:9000/wsPostWithIterators").post("ghostBody").map { response =>
     	val contentType = response.header("Content-Type").getOrElse("text/plain")
         Ok(response.body).as(contentType)
     }     
